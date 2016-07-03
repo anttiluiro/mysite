313deeaa2c68dbaf77abd01af882e507cbaf3796
Antti Luiro <anttiluiro@Anttis-MacBook-Pro.local>
2016-07-04 00:11:57 +0300

    Vagrant setup OK, polls app basics

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,13 +7,36 @@ apt-get update
 # set default python version to 3.4
 ln -sf /usr/bin/python3.4 /usr/bin/python
 
-# install pip
-apt-get install -y python3-pip
+# install pip (done later)
+# apt-get install -y python3-pip
+
+# Update and upgrade components, install new ones, restart postgresql
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get -y install python3-pip python3-dev libpq-dev postgresql postgresql-contrib
+service postgresql restart
+
+# Run db-creation sql
+# sudo su - postgres - psql < /vagrant/create_db.sql
+
+APP_DB_USER=anttiladmin
+APP_DB_PASS=tianp2e_MYSITE
+
+cat << EOF | su - postgres -c psql
+CREATE USER $APP_DB_USER WITH PASSWORD '$APP_DB_PASS';
+CREATE DATABASE mysite;
+
+ALTER ROLE anttiladmin SET client_encoding TO 'utf8';
+ALTER ROLE anttiladmin SET default_transaction_isolation TO 'read committed';
+ALTER ROLE anttiladmin SET timezone TO 'UTC';
+GRANT ALL PRIVILEGES ON DATABASE mysite TO anttiladmin;
+EOF
 
 # --- Required python modules ---
 pip3 install -r /vagrant/requirements.txt
+
 # tasks
-cd /vagrant && python manage.py syncdb --noinput
+cd /vagrant && python manage.py makemigrations
 cd /vagrant && python manage.py migrate
 
 # Run server and static file watcher in screen
--- manage.py
+++ manage.py
@@ -2,6 +2,8 @@
 import os
 import sys
 
+# First row points to wrong python location, is actually another in vagrant box
+# Might need to fix this if the django. commands here are needed
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
